6891725c4175400d4672fe40f3ef85d3cab7ab48
Родной мой <69079325+pa4H@users.noreply.github.com>
2022-09-04 13:43:01 +0700

    Add files via upload

---
+++ UEBANBooter/UEBAnBooter/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace UEBAnBooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 8);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(320, 254);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 268);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Confirmation";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(195, 268);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(137, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Automatically restart PC";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(344, 291);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UEBAN Booter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+
+    }
+}
+
---
+++ UEBANBooter/UEBAnBooter/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace UEBAnBooter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            checkBox1.Checked = Properties.Settings.Default.check1;
+            checkBox2.Checked = Properties.Settings.Default.check2;
+        }
+        string[] bootentry = new string[1];
+        string bcdedit;
+        string output;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bcdedit = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + @"\bcdedit.exe";
+            if (!File.Exists(bcdedit))
+            {
+                using (FileStream fs = new FileStream(bcdedit, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(Properties.Resources.bcdedit, 0, Properties.Resources.bcdedit.Length);
+                }
+            }
+            startBCDedit("/enum firmware");
+            // richTextBox1.AppendText(output);
+
+            int bootI = 0;
+
+            string[] mass = output.Split('\n');
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i].Contains("identifier") && !mass[i].Contains("identifier              {fwbootmgr}"))
+                {
+                    string a = mass[i];
+
+                    a = a.Replace("identifier", "");
+                    a = a.Replace(" ", "");
+
+                    int istart = a.IndexOf("{") + "{".Length;
+                    string s2 = a.Substring(istart, a.IndexOf("}") - istart);
+                    Array.Resize(ref bootentry, bootentry.Length + 1);
+                    bootentry[bootI] = s2;
+                    bootI++;
+                }
+                if (mass[i].Contains("description"))
+                {
+                    string a = mass[i];
+                    a = a.Replace("description", "");
+                    a = a.Replace("             ", "");
+                    listBox1.Items.Add(a);
+                }
+            }
+        }
+        DialogResult result;
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            //Text = listBox1.SelectedIndex.ToString();
+            
+            if (checkBox1.Checked) { result = MessageBox.Show(listBox1.SelectedItem.ToString(), "Boot into", MessageBoxButtons.YesNo, MessageBoxIcon.Question); }
+            if (result == DialogResult.Yes || !checkBox1.Checked)
+            {
+                if (listBox1.SelectedIndex >= 0)
+                {
+                    //MessageBox.Show(listBox1.SelectedItem.ToString() + "\n" + bootentry[listBox1.SelectedIndex], "Загрузиться в");
+                    //bcdedit.exe /set {fwbootmgr} bootsequence {8a391ab2-e167-11ea-81d7-806e6f6e6963} /addfirst
+                    //shutdown /r /t 0
+                    startBCDedit("/set {fwbootmgr} bootsequence {" + bootentry[listBox1.SelectedIndex] + "} /addfirst");
+
+                    if (checkBox2.Checked == true)
+                    {
+                        ProcessStartInfo shut = new ProcessStartInfo("shutdown", "/r /t 0");
+                        shut.UseShellExecute = false;
+                        Process.Start(shut);
+                    }
+                }
+            }
+        }
+        void startBCDedit(string args)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = bcdedit;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.Arguments = args;
+            process.Start();
+            output = process.StandardOutput.ReadToEnd();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                Properties.Settings.Default.check1 = true;
+            }
+            else { Properties.Settings.Default.check1 = false; }
+            Properties.Settings.Default.Save();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                Properties.Settings.Default.check2 = true;
+            }
+            else { Properties.Settings.Default.check2 = false; }
+            Properties.Settings.Default.Save();
+        }
+    }
+}
